3390b76a6548ff3cbd2d8d7dc1546de38736057a
Christian Kniep <christian@qnib.org>
2016-07-17 21:28:25 +0200

    working version

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM qnib/terminal
 
-RUN yum install -y bsdtar 
-ENV IPFS_VER=v0.3.11
-RUN curl -fsL https://gobuilder.me/get/github.com/ipfs/go-ipfs/cmd/ipfs/ipfs_${IPFS_VER}_linux-amd64.zip |bsdtar xf - -C /opt/ && \
-    mv /opt/ipfs/ipfs /usr/local/bin && chmod +x /usr/local/bin/ipfs && rm -rf /opt/ipfs
+RUN wget -qO /usr/local/bin/ipfs https://github.com/qnib/go-ipfs/releases/download/0.4.3-dev/ipfs-0.4.3-dev-x86 \
+ && chmod +x /usr/local/bin/ipfs
 ADD etc/supervisord.d/ipfs.ini /etc/supervisord.d/
 RUN useradd ipfs 
 VOLUME /data/ipfs/
--- opt/qnib/ipfs/bin/start.sh
+++ opt/qnib/ipfs/bin/start.sh
@@ -1,16 +1,7 @@
-#!/bin/bash 
+#!/bin/bash
 
 LOG_FILE=/var/log/supervisor/ipfs.log
-#### REDIRECT LOG
-# Close STDOUT file descriptor
-exec 1<&-
-# Close STDERR FD
-exec 2<&-
-# Open STDOUT as $LOG_FILE file for read and write.
-exec 1<>$LOG_FILE
-exec 2>&1
-
-export IPFS_LOGGING=debug 
+export IPFS_LOGGING=debug
 IPFS_DATA=${IPFS_DATA-/data/}
 cd ${IPFS_DATA}
 ipfs init
@@ -19,9 +10,9 @@ sleep 1
 ipfs bootstrap rm --all
 
 # smart to open it up so bad...?
-ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080 
+ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
 ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
-ipfs config SupernodeRouting.Servers --json "[]"
+#ipfs config SupernodeRouting.Servers --json "[]"
 ipfs config Datastore.Path /data/ipfs/
 
 ipfs daemon --routing dht
