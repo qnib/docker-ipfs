a31ae6ebae0c8f7581869973bdb8d36ef2a942f4
Christian Kniep <christian@qnib.org>
2016-07-17 21:35:01 +0200

    add ports and connect command

--- opt/qnib/ipfs/bin/start.sh
+++ opt/qnib/ipfs/bin/start.sh
@@ -12,7 +12,7 @@ ipfs bootstrap rm --all
 # smart to open it up so bad...?
 ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
 ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
-#ipfs config SupernodeRouting.Servers --json "[]"
+#ipfs swarm connect /ip4/104.236.176.52/tcp/4001/ipfs/qmsolnsgccfuzqjzradhn95w2crsfmzutddwp8hxahca9z
 ipfs config Datastore.Path /data/ipfs/
 
 ipfs daemon --routing dht
