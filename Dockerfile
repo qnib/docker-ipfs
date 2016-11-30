181a1cd74654e7446dae8e79b5147b2e79708d88
Christian Kniep <christian@qnib.org>
2016-11-30 22:22:31 +0100

    using private-network PR binary

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,16 @@ FROM qnib/alpn-consul
 
 #ENV IPFS_GATEWAY_ADDR=/ip4/0.0.0.0/tcp/8080
 #ENV IPFS_API_ADDR=/ip4/0.0.0.0/tcp/5001
-ENV IPFS_LOGGING=info
+ENV IPFS_LOGGING=info \
+    GATEWAY_PORT=8080 \
+    API_PORT=
 
 VOLUME /data/ipfs/
-RUN wget -qO /usr/local/bin/ipfs https://github.com/qnib/go-ipfs/releases/download/0.4.3/ipfs-0.4.3-MuslLinux \
+RUN wget -qO /usr/local/bin/ipfs https://github.com/qnib/go-ipfs/releases/download/0.4.5-pnet/ipfs-0.4.5-pnet_MuslLinux \
  && chmod +x /usr/local/bin/ipfs
 ADD etc/supervisord.d/ipfs.ini /etc/supervisord.d/
 ADD etc/consul.d/ipfs.json /etc/consul.d/
 ADD opt/qnib/ipfs/bin/start.sh \
     opt/qnib/ipfs/bin/check.sh \
     /opt/qnib/ipfs/bin/
+RUN echo 'tail -f /var/log/supervisor/ipfs.log' >> /root/.bash_history
--- opt/qnib/ipfs/bin/start.sh
+++ opt/qnib/ipfs/bin/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+MY_IP=$(ip route |tail -n1 |awk '{print $NF}')
 LOG_FILE=/var/log/supervisor/ipfs.log
 IPFS_DATA=${IPFS_DATA-/data/ipfs/}
 cd ${IPFS_DATA}
 ipfs init
 sleep 1
 ipfs config Datastore.Path ${IPFS_DATA}
-
 if [ "X${IPFS_BOOTSTRAP_RM_ALL}" == "Xtrue" ];then
     ipfs bootstrap rm --all
 fi
@@ -14,11 +14,20 @@ for peer in $(echo ${IPFS_BOOTSTRAP_PEERS} |sed -e 's/,/ /g');do
     ipfs bootstrap add ${peer}
 done
 
-if [ "X${IPFS_GATEWAY_ADDR}" != "X" ];then
-    ipfs config Addresses.Gateway ${IPFS_GATEWAY_ADDR}
-fi
-if [ "X${IPFS_API_ADDR}" != "X" ];then
-    ipfs config Addresses.API ${IPFS_API_ADDR}
+if [ "X${IPFS_BIND_IP}" != "X" ];then
+    ipfs config Addresses.Gateway /ip4/${IPFS_BIND_IP}/tcp/${GATEWAY_PORT}
+    ipfs config Addresses.API /ip4/${IPFS_BIND_IP}/tcp/${API_PORT}
+else
+    if [ "X${IPFS_GATEWAY_ADDR}" != "X" ];then
+        ipfs config Addresses.Gateway /ip4/${IPFS_GATEWAY_ADDR}/tcp/${GATEWAY_PORT}
+    else
+        ipfs config Addresses.Gateway /ip4/${MY_IP}/tcp/${GATEWAY_PORT}
+    fi
+    if [ "X${IPFS_API_ADDR}" != "X" ];then
+        ipfs config Addresses.API /ip4/${IPFS_API_ADDR}/tcp/${API_PORT}
+    else
+        ipfs config Addresses.API /ip4/${MY_IP}/tcp/${API_PORT}
+    fi
 fi
 
 ipfs daemon --routing dht
